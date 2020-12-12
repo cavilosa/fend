08046c60f189e627c1522c1e2a32d95ce06abd0d
calosa <cavilosa33@gmail.com>
2020-12-12 13:23:35 -0700

    Commit with no luck to work

--- projects/evaluate-news-nlp/src/client/js/cloud.js
+++
@@ -1,3 +0,0 @@
-function
-
-const url = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&of=json&txt=${text}&lang=en`;
--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,11 +3,9 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    //Client.checkForName(formText)
+    checkForName(formText)
 
     console.log("::: Form Submitted :::")
-
-    //generate("http://localhost:8080/test");
     fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
@@ -16,22 +14,3 @@ function handleSubmit(event) {
 }
 
 export { handleSubmit }
-
-/*// take the url as an input from the client and use meaningcloud api
-// with url as parameter to analize that.
-"https://api.meaningcloud.com/sentiment-2.1?key=<<YOUR OWN KEY>>&of=json&txt=<<YOUR OWN TXT>><MODEL>>&lang=en"
-*//*
- const generate = async () => {
-    const response = await fetch("http://localhost:8080/test");
-    if (response.status != 200) {
-        console.log(response.status);
-    }
-    try {
-        const data = await response.json();
-        return data;
-    } catch(error) {
-        console.log("error", error);
-    }
-
-
-}*/
